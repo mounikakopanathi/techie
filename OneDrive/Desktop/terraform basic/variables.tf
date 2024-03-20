5e2d75341b45466c0c37181807fdac1cbdd43d6f
mounika <mounikakopanathi1126@gmail.com>
2024-03-20 08:40:38 +0530

    terraform

---
+++ OneDrive/Desktop/terraform basic/variables.tf
@@ -0,0 +1,10 @@
+variable "filename" {
+    default = "pets.txt"
+    type = string
+    description = "filename of the Template "
+}
+variable "content" {
+    default = "I lost my pet "
+    type = string
+    description = "content of the file"
+}
